6c51771fa5e2e0f1b3f2db45c371ed6f9e433079
ASUKA.JP <ranuastikachiba@gmail.com>
2026-05-28 16:48:03 +0900

    Update bab-49.js

--- lessons/bab-49.js
+++ lessons/bab-49.js
@@ -1,10 +1,270 @@
 window.lessonData = window.lessonData || {};
 
 window.lessonData[49] = {
-  title: "Bab 49",
-  vocabulary: [],
-  patterns: String.raw`
+  title: "Bab 49: Kosa Kata",
 
-  `,
-  quiz: [],
+  vocabulary: [
+    {
+      japanese: "つとめます",
+      kanji: "勤めます",
+      meaning: "bekerja [untuk perusahaan]",
+      romaji: "tsutomemasu"
+    },
+    {
+      japanese: "やすみます",
+      kanji: "休みます",
+      meaning: "beristirahat",
+      romaji: "yasumimasu"
+    },
+    {
+      japanese: "かけます",
+      kanji: "掛けます",
+      meaning: "duduk [di kursi]",
+      romaji: "kakemasu"
+    },
+    {
+      japanese: "すごします",
+      kanji: "過ごします",
+      meaning: "menghabiskan (waktu)",
+      romaji: "sugoshimasu"
+    },
+    {
+      japanese: "よります",
+      kanji: "寄ります",
+      meaning: "mampir [ke bank]",
+      romaji: "yorimasu"
+    },
+    {
+      japanese: "いらっしゃいます",
+      kanji: "",
+      meaning: "ada, pergi, datang (bahasa hormat)",
+      romaji: "irasshaimasu"
+    },
+    {
+      japanese: "めしあがります",
+      kanji: "召し上がります",
+      meaning: "makan, minum (bahasa hormat)",
+      romaji: "meshiagarimasu"
+    },
+    {
+      japanese: "おっしゃいます",
+      kanji: "",
+      meaning: "mengatakan (bahasa hormat)",
+      romaji: "osshaimasu"
+    },
+    {
+      japanese: "なさいます",
+      kanji: "",
+      meaning: "melakukan (bahasa hormat)",
+      romaji: "nasaimasu"
+    },
+    {
+      japanese: "ごらんになります",
+      kanji: "ご覧になります",
+      meaning: "melihat (bahasa hormat)",
+      romaji: "goran ni narimasu"
+    },
+    {
+      japanese: "ごぞんじです",
+      kanji: "ご存じです",
+      meaning: "mengetahui (bahasa hormat)",
+      romaji: "gozonji desu"
+    },
+    {
+      japanese: "あいさつ",
+      kanji: "",
+      meaning: "salam",
+      romaji: "aisatsu"
+    },
+    {
+      japanese: "はいざら",
+      kanji: "灰皿",
+      meaning: "asbak",
+      romaji: "haizara"
+    },
+    {
+      japanese: "りょかん",
+      kanji: "旅館",
+      meaning: "penginapan ala Jepang",
+      romaji: "ryokan"
+    },
+    {
+      japanese: "かいじょう",
+      kanji: "会場",
+      meaning: "tempat pertemuan",
+      romaji: "kaijou"
+    },
+    {
+      japanese: "バスてい",
+      kanji: "バス停",
+      meaning: "halte bis",
+      romaji: "basutei"
+    },
+    {
+      japanese: "ぼうえき",
+      kanji: "貿易",
+      meaning: "perdagangan",
+      romaji: "boueki"
+    },
+    {
+      japanese: "〜さま",
+      kanji: "〜様",
+      meaning: "kata hormat untuk ～さん",
+      romaji: "~sama"
+    },
+    {
+      japanese: "かえりに",
+      kanji: "帰りに",
+      meaning: "pada waktu pulang",
+      romaji: "kaeri ni"
+    },
+    {
+      japanese: "たまに",
+      kanji: "",
+      meaning: "sesekali",
+      romaji: "tamani"
+    },
+    {
+      japanese: "ちっとも",
+      kanji: "",
+      meaning: "sama sekali (diikuti bentuk negatif)",
+      romaji: "chittomo"
+    },
+    {
+      japanese: "えんりょなく",
+      kanji: "遠慮なく",
+      meaning: "tanpa malu-malu",
+      romaji: "enryo naku"
+    },
+
+    // 会話
+    {
+      japanese: "いちねんいちくみ",
+      kanji: "一年一組",
+      meaning: "kelas 1-1",
+      romaji: "ichinen ichikumi"
+    },
+    {
+      japanese: "では",
+      kanji: "",
+      meaning: "kalau begitu",
+      romaji: "dewa"
+    },
+    {
+      japanese: "だします",
+      kanji: "出します",
+      meaning: "demam",
+      romaji: "dashimasu"
+    },
+    {
+      japanese: "よろしく おつたえください",
+      kanji: "よろしく お伝えください",
+      meaning: "Tolong sampaikan salam saya.",
+      romaji: "yoroshiku otsutae kudasai"
+    },
+    {
+      japanese: "しつれいいたします",
+      kanji: "失礼いたします",
+      meaning: "Permisi.",
+      romaji: "shitsurei itashimasu"
+    },
+
+    // 読み物
+    {
+      japanese: "こうし",
+      kanji: "講師",
+      meaning: "pembicara",
+      romaji: "koushi"
+    },
+    {
+      japanese: "おおくの〜",
+      kanji: "多くの〜",
+      meaning: "banyak ~",
+      romaji: "ooku no ~"
+    },
+    {
+      japanese: "さくひん",
+      kanji: "作品",
+      meaning: "karya",
+      romaji: "sakuhin"
+    },
+    {
+      japanese: "じゅしょうします",
+      kanji: "受賞します",
+      meaning: "menerima hadiah",
+      romaji: "jushou shimasu"
+    },
+    {
+      japanese: "せかいてきに",
+      kanji: "世界的に",
+      meaning: "seluruh dunia",
+      romaji: "sekaiteki ni"
+    },
+    {
+      japanese: "さっか",
+      kanji: "作家",
+      meaning: "pengarang",
+      romaji: "sakka"
+    },
+    {
+      japanese: "〜でいらっしゃいます",
+      kanji: "",
+      meaning: "adalah (bahasa hormat untuk です)",
+      romaji: "~de irasshaimasu"
+    },
+    {
+      japanese: "ちょうなん",
+      kanji: "長男",
+      meaning: "anak laki-laki tertua",
+      romaji: "chounan"
+    },
+    {
+      japanese: "しょうがい",
+      kanji: "障害",
+      meaning: "cacat",
+      romaji: "shougai"
+    },
+    {
+      japanese: "おもちです",
+      kanji: "お持ちです",
+      meaning: "mempunyai (bahasa hormat untuk もっています)",
+      romaji: "omochi desu"
+    },
+    {
+      japanese: "さっきょく",
+      kanji: "作曲",
+      meaning: "gubahan lagu, komposisi lagu",
+      romaji: "sakkyoku"
+    },
+    {
+      japanese: "かつどう",
+      kanji: "活動",
+      meaning: "kegiatan",
+      romaji: "katsudou"
+    },
+    {
+      japanese: "それでは",
+      kanji: "",
+      meaning: "baiklah",
+      romaji: "soredewa"
+    },
+    {
+      japanese: "おおえ けんざぶろう",
+      kanji: "大江 健三郎",
+      meaning: "pengarang novel Jepang (1935-)",
+      romaji: "Ooe Kenzaburou"
+    },
+    {
+      japanese: "とうきょうだいがく",
+      kanji: "東京大学",
+      meaning: "Universitas Tokyo",
+      romaji: "Toukyou Daigaku"
+    },
+    {
+      japanese: "ノーベルぶんがくしょう",
+      kanji: "ノーベル文学賞",
+      meaning: "Hadiah Nobel untuk Kesusastraan",
+      romaji: "Nooberu Bungakushou"
+    }
+  ]
 };
